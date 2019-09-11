cbbb0a82362b535bf15f1086ab43674ee45252f9
LucasBonilla <lcsbonilla@gmail.com>
2019-09-10 22:57:24 -0300

    [wip] cálculo do bearing

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ Para iniciar o ambiente virtual navegue até a raiz da aplicação e utilize
 
 > source venv/bin/activate
 
+- https://www.code-learner.com/python-read-big-file-example/
+- https://www.digitalocean.com/community/tutorials/how-to-index-and-slice-strings-in-python-3
+- https://www.w3schools.com/python/ref_string_replace.asp
+- https://www.movable-type.co.uk/scripts/latlong.html
+- https://github.com/geopy/geopy
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import requests
 import json
+from geopy import Point
+from geopy.distance import distance, VincentyDistance
 
 
 class MetaDados(object):
@@ -39,6 +41,82 @@ class Coordinates(object):
         return parsed_json["place_id"]
 
 
+class Bearing(object):
+
+    distance = None
+    bearing = None
+    latitude = None
+    longitude = None
+    remote_geoposition = None
+
+    def __init__(self, distance, bearing, latitude, longitude):
+        self.distance = distance
+        self.bearing = bearing
+        self.latitude = latitude
+        self.longitude = longitude
+        self.remote_latitude, self.remote_longitude = self.calculate_remote_point()
+        self.remote_geoposition = MetaDados(
+            self.remote_latitude, self.remote_longitude)
+
+    def calculate_remote_point(self):
+        point = VincentyDistance(kilometers=self.distance).destination(
+            Point(self.latitude, self.longitude), self.bearing)
+
+        return point.latitude, point.longitude
+
+
+def refactor(line):
+    return line.lower().replace(" ", "")
+
+
+def evaluate(coord, line):
+    to_return = None
+    if coord == 'la':
+        if 's' in line:
+            to_return = line.split('s')[1]
+        elif 'n' in line:
+            to_return = line.split('n')[1]
+    elif coord == 'lo':
+        if 'w' in line:
+            to_return = line.split('w')[1]
+        elif 'e' in line:
+            to_return = line.split('e')[1]
+    return to_return.replace("\n", "")
+
+
 if __name__ == "__main__":
     meta = MetaDados("-30.04982864", "-51.20150245")
     print(meta.geoposition.search_locale())
+    br = Bearing(22959, 137.352, -30.04982864, -51.20150245)
+    print(br.remote_latitude)
+    print(br.remote_longitude)
+
+    # file_path = 'data/data_points_20180101.txt'
+    # latitude = []
+    # latitude_tmp = None
+    # longitude = []
+    # longitude_tmp = None
+    # distance = []
+    # distance_tmp = None
+    # bearing = []
+    # bearing_tmp = None
+    # with open(file_path, 'r') as f:
+    #     while True:
+    #         line = f.readline()
+    #         if line:
+    #             line = refactor(line)
+    #             if 'latitude' in line:
+    #                 latitude_tmp = evaluate('la', line)
+    #             elif 'longitude' in line:
+    #                 longitude_tmp = evaluate('lo', line)
+    #             else:
+    #                 if not latitude_tmp or not longitude_tmp:
+    #                     print("deu ruim")
+    #                 latitude_tmp = None
+    #                 longitude_tmp = None
+    #                 distance_tmp = None
+    #                 bearing_tmp = None
+    #         else:
+    #             break
+    # print(len(latitude))
+    # print(len(longitude))
